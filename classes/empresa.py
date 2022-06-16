566e8f37906cb52b5219154f7f4c5a8d27b579c6
Kamila Alencar <kamilaalencar1998@gmail.com.(none)>
2022-06-16 17:59:20 -0300

    feat:criando metodos da classe empresa

--- classes/empresa.py
+++ classes/empresa.py
@@ -1,7 +1,10 @@
 import json
+import os
 from funcionario import Funcionario
 from datetime import datetime
-import os
+from gerente import Gerente
+
+
 
 
 class Empresa:
@@ -44,35 +47,70 @@ class Empresa:
             return True
         return False
 
+    def demissao(self, funcionario):
+        if funcionario in self.contratados:
+             self.contratados.pop(self.contratados.index(funcionario))
+        
+        else:
+            return "Não consta esse CPF na empresa"
 
-# empresa_1 = Empresa("  kenzie   brasil ", "12345678910124")
-# print(empresa_1.__dict__)
+        gerentes = [pessoa for pessoa in self.contratados if pessoa.funcao == "Gerente"]
 
-# print(len(empresa_1))
+        if funcionario.funcao == "Funcionário":
+            for gerente in gerentes:
+                if funcionario in gerente.funcionarios:
+                    gerente.funcionarios.pop(gerente.funcionarios.index(funcionario))
+                    return "Funcionário demitido"
+        else:
+            return "Gerente demitido" 
 
-# funcionario_1 = Funcionario(" jordan  cardoso poole ", "32112343215")
-# funcionario_2 = Funcionario("  stephen  alves curry ", "12332145665")
+    @staticmethod
+    
+    def ler_holerite(self, funcionario):
+        empresa = "_".join(self.nome.lower().split())
+        nome_funcionario = "_".join(funcionario.nome_completo.lower().split())
+        holerite = f"./empresas/{empresa}/{nome_funcionario}.json"
+        try:
+            return open(holerite, "r").read()
+        except:
+            return "Holerite não gerado!"
+        
+            
 
-# resposta = empresa_1.contratar_funcionario(funcionario_1)
-# empresa_1.contratar_funcionario(funcionario_2)
-# print(resposta) 
+empresa_1 = Empresa("  kenzie   brasil ", 12345678910124)
+funcionario_1 = Funcionario(" jordan  cardoso poole ", 32112343215)
+gerente_1 = Gerente(" bill    gates ", "32132186712")
+gerente_3 = Gerente("elon musk", "12342186574")
+# Adicionando funcionários
+empresa_1.contratar_funcionario(funcionario_1)
+empresa_1.contratar_funcionario(gerente_1)
+empresa_1.contratar_funcionario(gerente_3)
+# Adicionando funcionário ao gerente
+gerente_1.adicionar_funcionario(funcionario_1)
+# Funcionário não contratado
+funcionario_4 = Funcionario("klay mota thompson ", 92478965434)
 
-# print(f'CONTRATADOS: {len(empresa_1)}')
 
-# print(f'EMAIL: {funcionario_1.email}')
+empresa_1.gerar_holerite(funcionario_1)
+holerite = Empresa.ler_holerite(empresa_1 ,funcionario_1)
+print(holerite)
 
-# print(f'Empresa: {funcionario_1.empresa}')
 
-# resposta = empresa_1.contratar_funcionario(funcionario_2)
-# print(resposta) 
-
-# # Ao executar esse método deverá gerar o diretório e arquivo na pasta empresas 
-# holerite = empresa_1.gerar_holerite(funcionario_1)
-# print(holerite)
-# # True
-
-# # Funcionario não contratado
-# funcionario_3 = Funcionario("lamelo  ball souza ", "98778965434")
-# holerite = empresa_1.gerar_holerite(funcionario_3)
-# print(holerite)
-# # False
+print(len(empresa_1.contratados))
+# 3
+resposta = empresa_1.demissao(funcionario_4)
+print(resposta) 
+# Não consta esse CPF na empresa
+resposta = empresa_1.demissao(gerente_3) 
+print(resposta) 
+# Gerente demitido!
+print(len(gerente_1.funcionarios)) 
+# 1
+resposta = empresa_1.demissao(funcionario_1)
+print(resposta) 
+# Funcionário demitido!
+print(len(gerente_1.funcionarios)) 
+# 0
+print(len(empresa_1.contratados)) 
+#1
+ 
--- classes/gerente.py
+++ classes/gerente.py
@@ -1,5 +1,4 @@
 from funcionario import Funcionario
-from empresa import Empresa
 
 class Gerente(Funcionario):
     funcao = "Gerente"
@@ -18,37 +17,3 @@ class Gerente(Funcionario):
         self.funcionarios.append(funcionario)
         return True
 
-        
-
-
-gerente_1 = Gerente(" bill    gates ", "32132186712", 10000)
-print(gerente_1.__dict__)
-
-print(len(gerente_1))
-
-empresa_1 = Empresa("  kenzie    brasil ", "12345678910124")
-funcionario_1 = Funcionario(" jordan  cardoso poole ", "32112343215")
-gerente_2 = Gerente("steve  kerr ", "76532186123")
-
-# Contratando funcionários
-empresa_1.contratar_funcionario(funcionario_1)
-empresa_1.contratar_funcionario(gerente_1)
-empresa_1.contratar_funcionario(gerente_2)
-# Empresa diferente
-empresa_2 = Empresa(" golden state warriors  ", "12345678910111")
-funcionario_3 = Funcionario("lamelo  ball souza ", "98778965434")
-empresa_2.contratar_funcionario(funcionario_3)
-
-resposta = gerente_1.adicionar_funcionario(funcionario_1)
-print(resposta) 
-
-resposta = gerente_1.adicionar_funcionario(funcionario_1)
-print(resposta) 
-
-resposta = gerente_1.adicionar_funcionario(gerente_2)
-print(resposta) 
-
-resposta = gerente_1.adicionar_funcionario(funcionario_3)
-print(resposta)
-
-print(f'FUNCIONARIOS: {len(gerente_1)}')
